a485c3b305ac454dae215d3037b862046acc1434
progRayhan <prog.rayhan1@gmail.com>
2023-10-26 04:10:10 +0600

    feat: user can get list of current day restaurant menu

--- backend/menu/urls.py
+++ backend/menu/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from menu.views.current_day_menu import CurrentDayMenuView
 from menu.views.upload_menu import UploadMenuView
 
 urlpatterns = [
@@ -8,5 +9,11 @@ urlpatterns = [
         route="admin/upload-menu/",
         view=UploadMenuView.as_view(),
         name="upload_menu_for_restaurants",
-    )
+    ),
+    # http://0.0.0.0:8023/api/v1/menu/current-day-menu/
+    path(
+        route="current-day-menu/",
+        view=CurrentDayMenuView.as_view(),
+        name="current_day_menu",
+    ),
 ]
